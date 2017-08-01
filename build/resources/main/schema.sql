5ccfbff8156553997c8a3a7e31ff184383cf144b
Aman Sardana <asardana@crexendo.com>
2017-08-01 02:08:07 -0700

    commit with all the files

---
+++ build/resources/main/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS followers;
+
+-- Feel free to augment or modify these schemas (and the corresponding data) as you see fit!
+CREATE TABLE people (
+    id IDENTITY,
+    handle VARCHAR,
+    name VARCHAR,
+    username varchar(256),
+  	password varchar(256),
+);
+
+CREATE TABLE messages (
+    id IDENTITY,
+    person_id NUMBER REFERENCES people (id),
+    content VARCHAR
+);
+
+CREATE TABLE followers (
+    id IDENTITY,
+    person_id NUMBER REFERENCES people (id),
+    follower_person_id NUMBER REFERENCES people (id)
+);
